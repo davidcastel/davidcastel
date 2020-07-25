01ed82f0be7d41b7e723495a462e8cbcd698ba57
David Castellanos-Benito <47729944+davidcastel@users.noreply.github.com>
2020-07-24 20:10:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ exports.endpoint = function (req, resp) {
     resp.setHeader('Content-Type', 'image/svg+xml');
     resp.end(gradientBadge({
         subject: '',
+        icon: '',
         status: 'Instagram',
         style: 'flat',
         gradient: ['405de6', '5851db', '833ab4', 'c13584', 'e1306c', 'fd1d1d', 'ff00ee', 'f56040', 'f77737', 'fcaf45', 'ffdc80']
